0aaa27d3a8615dc49cf2e61c4436fd0383b72c50
Ferdinand Prantl <prantlf@gmail.com>
2016-01-17 14:38:03 +0100

    Create transpiled files

---
+++ dist/node/engine/cards/Ranks.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var Ranks={seven:"seven",eight:"eight",nine:"nine",ten:"ten",jack:"jack",queen:"queen",king:"king",ace:"ace"};Object.freeze(Ranks),exports["default"]=Ranks;
+//# sourceMappingURL=Ranks.js.map
---
+++ dist/node/engine/cards/Suits.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var Suits={hearts:"hearts",bells:"bells",acorns:"acorns",leaves:"leaves"};Object.freeze(Suits),exports["default"]=Suits;
+//# sourceMappingURL=Suits.js.map
